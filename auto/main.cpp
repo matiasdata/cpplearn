4ae3bec820229f5beddb83ecb1abc155f93439d5
matiasdata <matiasdata@gmail.com>
2025-07-03 08:05:37 +0100

    Update auto

--- auto/main.cpp
+++ auto/main.cpp
@@ -40,6 +40,7 @@ int main()
         std::cout << "Value: " << val << "\n";
     }
 
+    std::cout << "\n----- Closures -----\n";
     // Auto
     auto doubler = [](int n) { return n * 2; };
     std::cout << "Auto closure: " << doubler(10) << "\n";
@@ -49,6 +50,32 @@ int main()
     std::function<int(int)> doubler2 = [](int n) { return n * 2; };
     std::cout << "Manual closure: " << doubler2(10) << "\n";
     std::cout << "Type of doubler2: " << type_id_with_cvr<decltype(doubler2)>().pretty_name() << "\n";
+
+    auto w1 = std::make_unique<Widget>(Widget{10});
+    auto w2 = std::make_unique<Widget>(Widget{20});
+
+    auto derefLess = [](const auto& p1, const auto& p2) {return *p1 < *p2;};
+
+    std::cout << "derefLess: " <<  std::boolalpha << derefLess(w1, w2) << "\n";
+    std::cout << "Type of derefLess: " << type_id_with_cvr<decltype(derefLess)>().pretty_name() << "\n";
+
+    auto derefUPLess = [](const std::unique_ptr<Widget>& p1, 
+                        const std::unique_ptr<Widget>& p2) 
+                        {return *p1 < *p2;};
+
+    std::cout << "derefUPLess: " <<  std::boolalpha << derefUPLess(w1, w2) << "\n";
+    std::cout << "Type of derefUPLess: " << type_id_with_cvr<decltype(derefUPLess)>().pretty_name() << "\n";
+
+    std::function<bool(const std::unique_ptr<Widget>&,
+                     const std::unique_ptr<Widget>&)> 
+    derefUPLess2 = [](const std::unique_ptr<Widget>& p1,
+                     const std::unique_ptr<Widget>& p2) 
+                     {return *p1 < *p2;};
+
+    std::cout << "derefUPLess2: " <<  std::boolalpha << derefUPLess2(w1, w2) << "\n";
+    std::cout << "Type of derefUPLess2: " << type_id_with_cvr<decltype(derefUPLess2)>().pretty_name() << "\n";
+    
+    
 
 
 }
