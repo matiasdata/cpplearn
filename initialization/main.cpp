04588e48aac006a5fbb661234fd4e7edb07319cb
matiasdata <matiasdata@gmail.com>
2025-07-05 15:39:45 +0100

    Add Initialization

---
+++ initialization/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    int x(0); // initializer is in parentheses
+    int y = 0; // initializer follows "="
+    int z{0}; // initializer is in braces
+    int w = { 0 }; // rare: initializer uses "=" and braces
+}
