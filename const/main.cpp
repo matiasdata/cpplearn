733f50711dd3297a2cbff6d7f3e810ad6e4a3ed3
matiasdata <matiasdata@gmail.com>
2025-07-21 22:00:02 +0100

    Update const

--- const/main.cpp
+++ const/main.cpp
@@ -20,9 +20,33 @@ public:
         std::cout << "non-const operator[] call via cast..." << std::endl;
         return const_cast<char&>(static_cast<const TextBlock&>(*this)[position]);
     }
-    std::string get(){return text;}
+    std::string get()const {return text;}
 private:
     std::string text;
+};
+
+class CTextBlock
+{
+public:
+    CTextBlock(const char* pText_) {
+        if (!pText_) {
+            pText = nullptr;
+            textLength = 0;
+            lengthIsValid = false;
+        } else {
+            textLength = std::strlen(pText_);
+            lengthIsValid = true;
+            pText = new char[textLength + 1];
+            std::strcpy(pText, pText_);
+        }
+    }
+    char& operator[](std::size_t position) const {return pText[position];} //inappropriate, but bitwise const legitimate
+    std::string get() const {return static_cast<std::string>(pText);} 
+private:
+    char* pText{nullptr}; // a const CTextBlock could still modify the pText contents 
+    // because while the pointer will be const, it will not point to constant data.
+    std::size_t textLength{0};
+    bool lengthIsValid{false};
 };
 
 int main()
@@ -46,5 +70,10 @@ int main()
     // because we're trying to write through a const object.
     // ctb[1] = 'x';
 
+    const CTextBlock tbc("Hello");
+    std::cout << "tbc: " << tbc.get() << "\n";
+    std::cout << "Modifying tbc." << "\n";
+    tbc[1] = 'a';
+    std::cout << "Updated tbc: " << tbc.get() << "\n";
     return 0;
 }
