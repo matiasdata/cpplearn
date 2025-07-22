6dc6597091b736eec2cc8d3405bbdb67715e8c47
matiasdata <matiasdata@gmail.com>
2025-07-22 08:38:02 +0100

    Update const

--- const/main.cpp
+++ const/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class TextBlock
 {
@@ -25,6 +26,7 @@ private:
     std::string text;
 };
 
+
 class CTextBlock
 {
 public:
@@ -40,7 +42,13 @@ public:
             std::strcpy(pText, pText_);
         }
     }
-    char& operator[](std::size_t position) const {return pText[position];} //inappropriate, but bitwise const legitimate
+    char& operator[](std::size_t position) const {return pText[position];} 
+    //inappropriate, but bitwise const legitimate, to be appropriate its return 
+    // type should be const char&, so that in this way we can't modify the contents.
+    char& operator[](std::size_t position)
+    {
+        return const_cast<char&>(static_cast<const CTextBlock&>(*this)[position]);
+    }
     std::string get() const {return static_cast<std::string>(pText);} 
 private:
     char* pText{nullptr}; // a const CTextBlock could still modify the pText contents 
@@ -49,8 +57,41 @@ private:
     bool lengthIsValid{false};
 };
 
+
 int main()
 {
+
+    char greeting[] = "Hello, World";
+    char * pd = greeting; // array decays to char*, non-const pointer to non-const data
+    const char * pcd = greeting; // non-const pointer to const data, equivalent to char const * (it only matters that const is before *).
+    char * const cpd = greeting; // const pointer to non-const data
+    const char * const cpcd = greeting; // const pointer to const data, read from right to left.
+
+    pd = new char[] {"Hello"}; // legal to modify pointer
+    pd[1]='a'; // legal to modify contents
+    std::cout << "pd: " << pd << "\n";
+    pcd = new char[] {"Hello"}; // legal to modify pointer
+    // pcd[1] = 'a'; // illegal to modify contents
+    std::cout << "pcd: " << pcd << "\n";
+    // cpd = new char[6] {"Hello"}; // illegal to modify pointer
+    cpd[1] = 'a'; // legal to modify contents
+    std::cout << "cpd: " << cpd << "\n";
+    // cpcd = new char[]{"Hello"}; // illegal to modify pointer
+    // cpcd[1] = 'a'; // illegal to modify contents
+    std::cout << "cpcd: " << cpcd << "\n";
+
+    std::vector<int> vec {1,2,3,4,5};
+
+    const std::vector<int>::iterator it = vec.begin();
+    *it = 10; // legal, it behaves like a const-pointer to non-const data (i.e. T* const where T=int), can modify its contents but not the pointer.
+    // ++it; // illegal, behaves like a const-pointer to non-const data.
+    std::cout << "vec[0]= " << vec[0] << "\n";
+
+    std::vector<int>::const_iterator cit = vec.begin();
+    // *cit = 10; // illegal, behaves like a pointer to const data (i.e. const T*).
+    ++cit; // legal
+    std::cout << "*cit = " << *cit << "\n";
+
     TextBlock tb("Hello");
 
     std::cout << "Read from non-const object:\n";
@@ -73,7 +114,10 @@ int main()
     const CTextBlock tbc("Hello");
     std::cout << "tbc: " << tbc.get() << "\n";
     std::cout << "Modifying tbc." << "\n";
-    tbc[1] = 'a';
+    tbc[1] = 'a'; // notice that a constant CTextBlock can modify it's indirect contents because the pointer-to-char is of type
+    // char * const (pointer is const, address can't be modified, due to CTextBlock being const), but it is not of type
+    // const char * const, that would require the pointer-to-char being const char* by default, and would make its contents unmodifiable.
+    // We would still be able to replace the data as the pointer is not constant, but this would require copying and modifying the copy.
     std::cout << "Updated tbc: " << tbc.get() << "\n";
     return 0;
 }
