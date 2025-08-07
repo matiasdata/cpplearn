8d5a45bd3282c914504c9f7833d541f7ab4d0943
matiasdata <matiasdata@gmail.com>
2025-08-07 08:47:42 +0100

    Update item 11

--- item_11/main.cpp
+++ item_11/main.cpp
@@ -47,11 +47,17 @@ public:
 
     MyString& operator=(const char* other)
     {
-        if(str == other) return *this; // key line: handle self assignment in operator= (one could potentially be using the same char*)
-        delete[] str;
+        // if(str == other) return *this; // key line: handle self assignment in operator= (one could potentially be using the same char*)
+        // delete[] str;
+        // size_t len = strlen(other);
+        // str = new char[len+1];
+        // strcpy(str,other);
+        // return *this;
+        char* pOrig = str; // alternative: do a sort of swap, this can be exception safe if new throws an exception.
         size_t len = strlen(other);
         str = new char[len+1];
         strcpy(str,other);
+        delete[] pOrig;
         return *this;
     }
 
@@ -64,6 +70,40 @@ private:
     char* str;
 };
 
+template <typename T>
+class NamedObject
+{
+public: 
+    NamedObject() : name{}, obj{nullptr} {}
+    NamedObject(std::string name_, const T& obj_) : name{name_}, obj{new T(obj_)} {}
+    NamedObject(const NamedObject& other) : name{other.name}, obj{new T(*(other.obj))} {}
+    // NamedObject& operator=(const NamedObject& other)
+    // {
+    //     // alternative 3: copy and swap
+    //     NamedObject temp(other); // make a local (temporary) copy of other's data
+    //     swap(temp); // swap *this's data with the copy's data; needs a swap member function.
+    //     return *this;
+    // }
+    void swap(NamedObject& other)
+    {
+        std::swap(name, other.name);
+        std::swap(obj,other.obj);
+    }
+    NamedObject& operator=(NamedObject other)
+    {
+        // alternative 4: more concise copy and swap
+        swap(other); // since other was passed by value, it is already a copy
+        return *this;
+    }
+    // one can declare only one operator=
+    const std::string& getName(){return name;}
+    const T& getObj(){return *obj;}
+
+private:
+    std::string name;
+    T* obj;
+};
+
 
 int main()
 {
@@ -74,6 +114,16 @@ int main()
     std::cout << s.get() << "\n";
     s = t;
     std::cout << s.get() << "\n";
+    s = "Now my name is Slim Shady!";
+    std::cout << s.get() << "\n";
+
+    NamedObject<int> me("Matias",34);
+    std::cout << "me: " << me.getName() << ", age: " << me.getObj() << "\n"; 
+    NamedObject<int> mom("Viviana",75);
+    std::cout << "mom: " << mom.getName() << ", age: " << mom.getObj() << "\n"; 
+    me = mom;
+    std::cout << "me: " << me.getName() << ", age: " << me.getObj() << "\n"; 
+
 }
 
 /*
@@ -81,6 +131,6 @@ Key Takeaways:
     * Make sure operator= is well-behaved when an object is assigned to
     itself. Techniques include comparing addresses of source and target
     objects, careful statement ordering, and copy-and-swap.
-    * Make sure that any function operating on more than one object be-
-    haves correctly if two or more of the objects are the same.
+    * Make sure that any function operating on more than one object behaves
+     correctly if two or more of the objects are the same.
 */
