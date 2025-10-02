4f78c9048622979f5e28605b3bb714368fd50cef
matiasdata <matiasdata@gmail.com>
2025-10-02 08:37:46 +0100

    update item 31

--- item_31/date.cpp
+++ item_31/date.cpp
@@ -4,6 +4,6 @@ Date::Date(int d, int m, int y) : day{d}, month{m}, year{y} {}
 
 std::ostream& operator<<(std::ostream& os, const Date& d)
 {
-    os << d.day << "/" << d.month << "/" << d.year;
+    os << d.getDay() << "/" << d.getMonth() << "/" << d.getYear();
     return os;
 }
