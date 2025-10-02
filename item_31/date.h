f37361b03aebf051b5733a5d28259ef3bceefc91
matiasdata <matiasdata@gmail.com>
2025-10-02 08:37:46 +0100

    update item 31

--- item_31/date.h
+++ item_31/date.h
@@ -6,10 +6,14 @@ class Date
 {
 public:
     Date(int d, int m, int y);
-    friend std::ostream& operator<<(std::ostream& os, const Date& d);
+    int getDay() const {return day;}
+    int getMonth() const {return month;}
+    int getYear() const {return year;}
 
 private:
     int day;
     int month;
     int year;
 };
+
+std::ostream& operator<<(std::ostream& os, const Date& d);
