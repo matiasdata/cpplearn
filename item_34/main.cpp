ed2ebe21a848103194cf3c3d5e9a099feef24846
matiasdata <matiasdata@gmail.com>
2025-10-14 21:23:25 +0100

    Update item 34

--- item_34/main.cpp
+++ item_34/main.cpp
@@ -6,7 +6,9 @@ public:
     Airplane() : planeId{counter} {counter++;}
     virtual void construct() = 0;
     virtual void fly() = 0;
-    int planeID() const {return planeId;};
+    int planeID() const {return planeId;}
+// protected:    
+//      void defaultFly(){std::cout << "Airplane default fly.\n";}
 private:
     int planeId;
     static int counter;
@@ -14,6 +16,7 @@ private:
 
 int Airplane::counter = 0;
 
+// define a default implementation for fly, this can be called by derived classes (explicitely) if they need a default implementation.
 void Airplane::fly()
 {
     std::cout << "Airplane default fly.\n";
@@ -36,7 +39,7 @@ public:
 class ModelC : public Airplane
 {
 public:
-    void construct() {std::cout << "Constructing model C\n.";}
+    void construct() {std::cout << "Constructing model C.\n";}
     void fly(){std::cout << "Model C fly.\n";}
 };
 
@@ -60,3 +63,24 @@ int main()
     std::cout << "Airplane_A2 id: " << airplane_A2.planeID() << "\n";
 
 }
+
+/*
+
+Key takeaways:
+    * Inheritance of interface is different from inheritance of implementation.
+    Under public inheritance, derived classes always inherit base interfaces.
+    * Pure virtual functions specify inheritance of interface only.
+    * Simple (impure) virtual functions specify inheritance of interface
+    plus inheritance of a default implementation.
+    * Non-virtual functions specify inheritance of interface plus inheritance
+    of a mandatory implementation.
+
+In order to prevent erroneously assign the default implementation of a virtual member
+function to a derived class, one can declare all virtual functions as pure virtual
+and nevertheless provide a default implementation. Then on each derived class, one 
+needs to call the base class function (in this example we call Airplane::fly(), notice
+the need to specify we refer to the base one). Alternatively, we can declare our member
+function as pure virtual, and declare protected a member function that provides a default
+implementation, and then derived classes can call this function if they want to use the 
+default version.
+*/
